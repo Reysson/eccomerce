73fb9024562ab55ef3bf1678681c3d59052434eb
Reysson Martins <reysson.martins30@gmail.com>
2022-01-30 14:44:20 -0400

    refactor creating KafkaService

--- eccomerce/src/main/java/com/reysson/eccomerce/EmailService.java
+++ eccomerce/src/main/java/com/reysson/eccomerce/EmailService.java
@@ -1,45 +1,29 @@
 package com.reysson.eccomerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(getProperties());
-        consumer.subscribe(Collections.singletonList("ECCOMERCE_SEND_EMAIL"));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("econtrei "+records.count()+ " registros");
-                for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("sending email");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+        var emailService = new EmailService();
+        var service = new KafkaService(EmailService.class.getSimpleName(),"ECCOMERCE_SEND_EMAIL",emailService::parse);
+        service.run();
+
     }
 
-    private static Properties getProperties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        return properties;
+    private void parse(ConsumerRecord<String,String> records) {
+        System.out.println("------------------------------------------");
+        System.out.println("sending email");
+        System.out.println(records.key());
+        System.out.println(records.value());
+        System.out.println(records.partition());
+        System.out.println(records.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Email sending.");
     }
+
 }
--- eccomerce/src/main/java/com/reysson/eccomerce/FraudeDetectorService.java
+++ eccomerce/src/main/java/com/reysson/eccomerce/FraudeDetectorService.java
@@ -1,49 +1,29 @@
 package com.reysson.eccomerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudeDetectorService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(getProperties());
-        consumer.subscribe(Collections.singletonList("ECCOMERCE_NEW_ORDER"));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("econtrei "+records.count()+ " registros");
-                for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud.");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed.");
-                }
-            }
-        }
+        var fraudeDetectorService = new FraudeDetectorService();
+        var service = new KafkaService(FraudeDetectorService.class.getSimpleName(),"ECCOMERCE_NEW_ORDER", fraudeDetectorService::parse);
+        service.run();
+
     }
 
-    private static Properties getProperties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudeDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudeDetectorService.class.getSimpleName()+" - "+ UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");//consumo 1 por vez, caso tenha muitas mensagens
-        return properties;
+    private void parse(ConsumerRecord<String,String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud.");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Order processed.");
     }
+
 }
